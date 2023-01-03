9ba87241dbe98226841c1fa5949c48b0de4e851d
arijit-roy3 <arijit.roy@perfios.com>
2023-01-03 13:16:17 +0530

    jwt without repo

---
+++ src/main/java/com/dailycodebuffer/jwt/config/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package com.dailycodebuffer.jwt.config;
+
+import com.dailycodebuffer.jwt.filter.JwtFilter;
+import com.dailycodebuffer.jwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    //this is the security configuration class which extends websecurity configuration
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //here we are using the userdetails service
+        //passing the reference of the userservice here to get the user details
+        auth.userDetailsService(userService);
+    }
+
+    //this is used to authenticate the user
+    //implementaion is provided by spring security core
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    //we overrride the configure methods
+    //we are blocking all the routes except 'authenticate'
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);//adding the filter here
+
+    }
+}
